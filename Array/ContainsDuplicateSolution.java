1e0f718088e419ae0592b3b0ed2e5bfa52401e88
nancywan_yumebau <nancy.wan@yumebau.com>
2023-05-18 08:34:31 -0700

    Adding problem number

--- Array/ContainsDuplicateSolution.java
+++ Array/ContainsDuplicateSolution.java
@@ -1,4 +1,4 @@
-//Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+// 217. Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
 import java.util.*;
 // Hashset Solution 
 // TC: O(n)
--- Array/ValidAnagramSolution.java
+++ Array/ValidAnagramSolution.java
@@ -1,4 +1,4 @@
-// Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+// 242. Given two strings s and t, return true if t is an anagram of s, and false otherwise.
 // An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
 
 // Using an array to store character occurrences of two strings and see if the occurrences are all zeros
